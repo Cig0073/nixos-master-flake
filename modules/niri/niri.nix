18d9ea800a5a74b26b771df9ed160efae7a1f1f3
cig0073 <cig0073@protonmail.com>
2026-09-26 01:32:54 +0300

    fix dms

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -16,12 +16,6 @@ in
       enable = true;             # Systemd service for auto-start
       restartIfChanged = true;   # Auto-restart dms.service when dms-shell changes
     };
-  
-    # Core features
-    enableVPN = true;                  # VPN management widget
-    enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
-    enableAudioWavelength = true;      # Audio visualizer (cava)
-    enableCalendarEvents = true;       # Calendar integration (khal)
   };
 
   services.displayManager.dms-greeter = {
